46288fb7bcc0a8d4906e12f15ee70f21ebb014bf
HipsterBrown <headhipster@hipsterbrown.com>
2023-01-09 17:52:02 -0500

    feat: export LitraController class as separate module from CLI

---
+++ bin/cli.mjs
@@ -0,0 +1,83 @@
+import { parseArgs } from 'node:util'
+import { LitraController, TEMP_MAX, TEMP_MIN } from '../index.mjs'
+
+const options = {
+  help: {
+    type: 'boolean',
+    short: 'h'
+  },
+  info: {
+    type: 'boolean',
+    short: 'i'
+  },
+  on: {
+    type: 'boolean',
+  },
+  off: {
+    type: 'boolean',
+  },
+  brightness: {
+    type: 'string',
+    short: 'b'
+  },
+  temp: {
+    type: 'string',
+    short: 't'
+  }
+}
+const { values, positionals } = parseArgs({ options, allowPositionals: true })
+
+if (values.help || positionals.includes('help') || (Object.keys(values).length === 0 && positionals.length === 0)) {
+  console.log('Welcome to Litra Controller!')
+  console.log(`
+Use this CLI to control your Logitech Litra Glow!
+--help, -h:            Get info about commands and various controls
+--info, -i:            Get info about the device, if connected
+--on:                  Turn on the light
+--off:                 Turn off the light
+--brightness, -b:      Set the brightness of the light, 0-100
+--temp, -t:            Set the temperature of the light (color shade), between ${TEMP_MIN} and ${TEMP_MAX}
+`)
+  process.exit(0)
+}
+
+const glow = new LitraController()
+
+if (values.info || positionals.includes('info')) {
+  const info = glow.getInfo()
+  console.table(info)
+  process.exit(0)
+}
+
+if (values.on || positionals.includes('on')) {
+  glow.on();
+  console.log(`Let there be light!`)
+}
+
+if (values.off || positionals.includes('off')) {
+  glow.off()
+  console.log(`Nothing to see here.`)
+}
+
+if (values.brightness) {
+  const level = parseInt(values.brightness, 10)
+  if (level >= 0 || level <= 100) {
+    glow.setBrightness(level)
+    console.log(`Brightness set to ${level}%`)
+  } else {
+    console.error('Brightness level must be between 0 and 100')
+    process.exit(1)
+  }
+}
+
+if (values.temp) {
+  const temp = parseInt(values.temp, 10)
+  if (temp >= TEMP_MIN || temp <= TEMP_MAX) {
+    glow.setTemperature(temp)
+    console.log(`Temperature set to ${temp}K`)
+  } else {
+    console.error(`Temperature setting must be between ${TEMP_MIN} and ${TEMP_MAX}`)
+    process.exit(1)
+  }
+}
+
--- index.mjs
+++ index.mjs
@@ -1,128 +1,49 @@
-import { parseArgs } from 'node:util'
 import { HID } from 'node-hid'
-
-const options = {
-  help: {
-    type: 'boolean',
-    short: 'h'
-  },
-  info: {
-    type: 'boolean',
-    short: 'i'
-  },
-  on: {
-    type: 'boolean',
-  },
-  off: {
-    type: 'boolean',
-  },
-  brightness: {
-    type: 'string',
-    short: 'b'
-  },
-  temp: {
-    type: 'string',
-    short: 't'
-  }
-}
-const { values, positionals } = parseArgs({ options, allowPositionals: true })
-
-if (values.help || positionals.includes('help') || (Object.keys(values).length === 0 && positionals.length === 0)) {
-  console.log('Welcome to Litra Controller!')
-  console.log(`
-Use this CLI to control your Logitech Litra Glow!
---help, -h:            Get info about commands and various controls
---info, -i:            Get info about the device, if connected
---on:                  Turn on the light
---off:                 Turn off the light
---brightness, -b:      Set the brightness of the light, 0-100
---temp, -t:            Set the temperature of the light (color shade), between ${TEMP_MIN} and ${TEMP_MAX}
-`)
-  process.exit(0)
-}
 
 const VENDOR_ID = 0x046d
 const PID_GLOW = 0xc900
-// const PID_BEAM = 0xc901
 const LIGHT_OFF = 0x00
 const LIGHT_ON = 0x01
 const BRIGHTNESS_MIN = 0x14
 const BRIGHTNESS_MAX = 0xfa
-const TEMP_MAX = 6500
-const TEMP_MIN = 2700
+export const TEMP_MAX = 6500
+export const TEMP_MIN = 2700
+const ENTRY_BYTES = [0, 0x11, 0xff, 0x04]
+const POWER_BYTES = [0x1c]
+const BRIGHTNESS_BYTES = [0x4c, 0x00]
+const TEMP_BYTES = [0x9c]
 
-const glow = new HID(VENDOR_ID, PID_GLOW)
+export class LitraController {
+  #device;
 
-if (values.info || positionals.includes('info')) {
-  const info = glow.getDeviceInfo()
-  console.table(info)
-  process.exit(0)
-}
-
-if (values.on || positionals.includes('on')) {
-  on();
-}
-
-if (values.off || positionals.includes('off')) {
-  off()
-}
-
-if (values.brightness) {
-  const level = parseInt(values.brightness, 10)
-  if (level >= 0 || level <= 100) {
-    setBrightness(level)
-  } else {
-    console.error('Brightness level must be between 0 and 100')
-    process.exit(1)
+  constructor() {
+    this.#device = new HID(VENDOR_ID, PID_GLOW)
   }
-}
 
-if (values.temp) {
-  const temp = parseInt(values.temp, 10)
-  if (temp >= TEMP_MIN || temp <= TEMP_MAX) {
-    setTemp(temp)
-  } else {
-    console.error(`Temperature setting must be between ${TEMP_MIN} and ${TEMP_MAX}`)
-    process.exit(1)
+  getInfo() {
+    return this.#device.getDeviceInfo()
   }
-}
 
-function on() {
-  glow.write([0,
-    0x11, 0xff, 0x04, 0x1c, LIGHT_ON,
-    0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00])
-}
+  on() {
+    this.#device.write(ENTRY_BYTES.concat(POWER_BYTES, [LIGHT_ON], new Array(15).fill(0)))
+  }
 
-function off() {
-  glow.write([0,
-    0x11, 0xff, 0x04, 0x1c, LIGHT_OFF,
-    0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00])
-}
+  off() {
+    this.#device.write(ENTRY_BYTES.concat(POWER_BYTES, [LIGHT_OFF], new Array(15).fill(0)))
+  }
 
-function setBrightness(level) {
-  const adjustedLevel = Math.floor(BRIGHTNESS_MIN + ((level / 100) * (BRIGHTNESS_MAX - BRIGHTNESS_MIN)))
+  setBrightness(level) {
+    const adjustedLevel = Math.floor(BRIGHTNESS_MIN + ((level / 100) * (BRIGHTNESS_MAX - BRIGHTNESS_MIN)))
+    this.#device.write(ENTRY_BYTES.concat(BRIGHTNESS_BYTES, [adjustedLevel], new Array(14).fill(0)))
+  }
 
-  glow.write([0,
-    0x11, 0xff, 0x04, 0x4c, 0x00,
-    adjustedLevel, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00])
-  console.log(`Brightness level set to ${level}%`)
-}
+  setTemperature(temp) {
+    const adjustedTemp = temp.toString(16).padStart(4, '0').toUpperCase()
 
-function setTemp(temp) {
-  const adjustedTemp = temp.toString(16).padStart(4, '0').toUpperCase()
+    const temp1 = parseInt(adjustedTemp.slice(0, 2), 16)
+    const temp2 = parseInt(adjustedTemp.slice(2), 16)
 
-  const temp1 = parseInt(adjustedTemp.slice(0, 2), 16)
-  const temp2 = parseInt(adjustedTemp.slice(2), 16)
+    this.#device.write(ENTRY_BYTES.concat(TEMP_BYTES, [temp1, temp2], new Array(14).fill(0)))
 
-  glow.write([0,
-    0x11, 0xff, 0x04, 0x9c, temp1,
-    temp2, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00])
+  }
 }
